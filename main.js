0502b7b24a5f2179d4a29590ebdeee7446b0f5bb
Mystic <mystic189123@gmail.com>
2024-09-16 13:17:11 +0300

    Website

---
+++ main.js
@@ -0,0 +1,12 @@
+function HeartChange(id, heartFull, heartNormal) {
+    const element = document.getElementById(id);
+    const currentState = element.getAttribute('data-state');
+
+    if (currentState === 'normal') {
+        element.src = heartFull;
+        element.setAttribute('data-state', 'full');
+    } else {
+        element.src = heartNormal;
+        element.setAttribute('data-state', 'normal');
+    }
+}
